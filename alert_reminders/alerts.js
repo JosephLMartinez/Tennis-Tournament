9e99e069f7268b57755b53e8cc76a9b6284b4692
Margo <124205380+Marisanti42@users.noreply.github.com>
2023-05-24 22:04:11 -0500

    hardcoded alert box example

--- alert_reminders/alerts.js
+++
@@ -1,8 +0,0 @@
-function tournamentMsg(msg) {
-    var myName = "Joe"
-    alert(msg);
-}
-
-
-//variable has to be declared as in var my_name = "{{ session['my_name']}}";
-//would it work without the outside double quotes? or not because its python being called in by js?
